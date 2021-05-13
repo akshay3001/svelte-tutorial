0e7845b1dd6a3fced7f067ae5216f32af48f32f6
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2021-05-13 21:47:42 +0530

    [binding-class]: How to apply class, dynamically

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,31 @@
 <script>
-	const name = 'Test'
+	const name = 'Test';
+	const success = 'success';
+	const isSelected = true;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h2 class='primary'>Primary</h2>
+	<h2 class={success}>Success</h2>
+	<h2 class={isSelected ? 'isSelected' : ''}>Selected</h2>
+	Since both the condition variable and class names are same Svelte has a syntax for it:
+	<h2 class:isSelected>Selected</h2>
 </main>
 
 <style>
+	.primary {
+		color: blue;
+	}
+
+	.success {
+		color: green;
+	}
+
+	.isSelected {
+		color: yellow;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
