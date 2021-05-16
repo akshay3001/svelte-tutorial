473e92d6bb8dc341f9b5bca04b4b428c354d96f5
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2021-05-16 20:18:01 +0530

    [conditional-rendering]: if, if else, if else if

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,35 @@
 <script>
-	const name = 'Test'
+	const num = 10;
+	let users = [];
+	// let users = [
+	// 	{name: 'John', id: 1},
+	// 	{name: 'Doe', id: 2},
+	// 	{name: 'David', id: 3},
+	// ]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	{#if num === 0}
+		 <h2>Number is 0</h2>
+		 {:else if  num < 0}
+			  <h2>Number is less than 0</h2>
+				{:else if num > 0}
+					<h2>Number is greater than 0</h2>
+					{:else}
+						<h2>Is not a number</h2>
+	{/if}
+	<hr>
+
+	{#if users.length}
+			<h2>List of Users:</h2>
+			<ul>
+				{#each users as user}
+				 	<li>{user.name}</li>
+				{/each}
+			</ul>
+			{:else}
+				<h2>No users found</h2>
+	{/if}
 </main>
 
 <style>
