dd10cd2bd939f688e7feca9e6a46375b69a0f6a0
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2021-05-18 23:28:01 +0530

    [event-binding]: Events examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,39 @@
 <script>
-	const name = 'Test'
+  let counter = 0;
+  const onCounterUpdate = (type = "increment", step = 1) => {
+    console.log({ type });
+    console.log({ step });
+    counter = type === "increment" ? counter + 1 : counter - 1;
+  };
+  const onMouseOver = (e) => console.log(e);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <h1>Counter: {counter}</h1>
+  <button on:click={() => onCounterUpdate("increment", 5)}>+</button>
+  <button on:click={() => onCounterUpdate("decrement", 5)}>-</button>
+  <hr />
+  <button on:mouseover={onMouseOver}>Mouse over me</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
