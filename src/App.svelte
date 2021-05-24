79f02eed808bc1a1c6e2ea5ba6b06b4f4f91f3c6
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2021-05-24 19:27:10 +0530

    [props]: Component props in Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,35 @@
 <script>
-	const name = 'Test'
+  import Hello from "./Hello.svelte";
+  const user = {
+    username: "John Doe",
+    lastLoggedIn: "yesterday",
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <Hello username="Test User" lastLoggedIn="yesterday" />
+  <hr />
+  <Hello {...user} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/Hello.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let username;
+  export let lastLoggedIn = "today";
+</script>
+
+<h2>Hello {username}!!</h2>
+<p>Last logged in {lastLoggedIn}</p>
