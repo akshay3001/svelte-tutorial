8156fa073d8f234ed9efa6a012b683e652d9b63d
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2021-05-26 21:07:47 +0530

    [comp-events]: createEventDispatcher example

---
+++ src/Child.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  function dispatcher() {
+    dispatch("child-clicked", {
+      clicked: true,
+    });
+  }
+</script>
+
+<h2>Child Component</h2>
+<button on:click={dispatcher}>Click Me</button>
