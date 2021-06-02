d2ce4d074587ab1736ef74fe779204517ff91b05
Akshay <akshay.mhatre8989@gmail.com>
2021-06-02 19:06:10 +0530

    [comp-style]: Scoped comp styling and global

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,39 @@
 <script>
-	const name = 'Test'
+  import Heading from "./Heading.svelte";
+
+  const name = "Test";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <!-- Component styles are scoped by default in Svelte -->
+  <!-- Meaning, styles applied at component will be applied to that component itself -->
+  <!-- To make Global styling available make use of :global() -->
+  <h2>Hello {name}!</h2>
+  <h3>Hello {name}!</h3>
+  <Heading />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  :global(h3) {
+    color: blue;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h2 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/Heading.svelte
@@ -0,0 +1,8 @@
+<h2>Heading Component works!</h2>
+<h3>Heading Component works!</h3>
+
+<style>
+  h2 {
+    color: green;
+  }
+</style>
